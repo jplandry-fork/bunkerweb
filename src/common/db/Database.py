e855ad36b5d472d9fa887bed81b532e5e5d0b3fd
TheophileDiot <tdiot@bunkerity.com>
2026-01-19 10:26:49 +0100

    fix: Correct plugin deletion logic to identify manually installed plugins for proper removal

--- src/common/db/Database.py
+++ src/common/db/Database.py
@@ -1037,7 +1037,7 @@ class Database:
             # Plugins to delete
             for pid in old_plugin_ids - new_plugin_ids:
                 old_p = old_plugins[pid]
-                if old_p.type == "core":
+                if old_p.method == "manual":
                     self.logger.warning(f'{old_p.type.title()} plugin "{pid}" has been removed, deleting it')
                     to_delete.append({"type": "plugin", "filter": {"id": pid}})
 
